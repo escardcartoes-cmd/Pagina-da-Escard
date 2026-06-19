45d7937d7d13f8c4b9fe1ab98f85ce7d2b443c66
ES CARD <escardcartoes@gmail.com>
2026-06-19 12:40:00 -0300

    remove functions

--- functions/api/admin/logout.js
+++
@@ -1,9 +0,0 @@
-// functions/api/admin/logout.js
-import { setCookieHeader, json } from "../../_lib/auth.js";
-
-export async function onRequestPost() {
-  return json(
-    { ok: true },
-    { headers: { "Set-Cookie": setCookieHeader("", 0) } }
-  );
-}
--- functions/api/admin/session.js
+++
@@ -1,9 +0,0 @@
-// functions/api/admin/session.js
-import { isAuthed, json } from "../../_lib/auth.js";
-
-export async function onRequestGet({ request, env }) {
-  return json({
-    authed: await isAuthed(request, env),
-    configured: Boolean(env.ADMIN_PASSWORD),
-  });
-}
